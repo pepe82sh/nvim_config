315473fa99f7280ae68c0bb401455ea56f81e0c1
pepe <git@pe82.de>
2024-03-30 14:57:51 +0100

    Enable reverse tabbing

--- lua/custom/plugins/barbar_tabs.lua
+++ lua/custom/plugins/barbar_tabs.lua
@@ -1,5 +1,6 @@
 -- use tab to change tabs
 vim.keymap.set('n', '<Tab>', '<Cmd>BufferNext<CR>', { desc = 'Go to next buffer' })
+vim.keymap.set('n', '<S-Tab>', '<Cmd>BufferPrevious<CR>', { desc = 'Go to next buffer' })
 
 -- This plugin enables tabs in neovim
 return {
@@ -8,7 +9,9 @@ return {
     'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
     'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
   },
-  init = function() vim.g.barbar_auto_setup = false end,
+  init = function()
+    vim.g.barbar_auto_setup = false
+  end,
   opts = {
     -- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
     -- animation = true,
@@ -16,4 +19,3 @@ return {
     -- …etc.
   },
 }
-
