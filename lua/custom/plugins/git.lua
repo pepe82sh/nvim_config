8e90b536c6c7af98c9a06d444c2e5fd094f8583c
pepe <git@pe82.de>
2024-04-08 22:37:47 +0200

    add nvim plugin vim-fugitive

--- lua/custom/plugins/git.lua
+++ lua/custom/plugins/git.lua
@@ -3,6 +3,11 @@
 --    require('gitsigns').setup({ ... })
 --
 -- See `:help gitsigns` to understand what the configuration keys do
+-- Here is a more advanced example where we pass configuration
+-- options to `gitsigns.nvim`. This is equivalent to the following Lua:
+--    require('gitsigns').setup({ ... })
+--
+-- See `:help gitsigns` to understand what the configuration keys do
 return
 -- Here is a more advanced example where we pass configuration
 -- options to `gitsigns.nvim`. This is equivalent to the following Lua:
@@ -10,14 +15,19 @@ return
 --
 -- See `:help gitsigns` to understand what the configuration keys do
 { -- Adds git related signs to the gutter, as well as utilities for managing changes
-  'lewis6991/gitsigns.nvim',
-  opts = {
-    signs = {
-      add = { text = '+' },
-      change = { text = '~' },
-      delete = { text = '_' },
-      topdelete = { text = '‾' },
-      changedelete = { text = '~' },
+  {
+    'lewis6991/gitsigns.nvim',
+    opts = {
+      signs = {
+        add = { text = '+' },
+        change = { text = '~' },
+        delete = { text = '_' },
+        topdelete = { text = '‾' },
+        changedelete = { text = '~' },
+      },
     },
+  },
+  {
+    'tpope/vim-fugitive',
   },
 }
